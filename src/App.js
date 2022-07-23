8d0ff5a766ff6eca3eb645a34c41f6e6900863ff
SalmonFillet <sy4.l3ucas@gmail.com>
2022-07-23 10:50:23 +0900

    ✨ fetch api

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { useEffect } from "react";
+import { getAllPokemon } from "./utils/pokemon";
 import "./App.css";
 
 export const App = () => {
   const initialURL = "https://pokeapi.co/api/v2/pokemon";
+
+  useEffect(() => {
+    const fetchPokemonData = async () => {
+      //fetch all poke data
+      const res = await getAllPokemon(initialURL);
+      console.log(res);
+    };
+    fetchPokemonData();
+  }, []);
 };
