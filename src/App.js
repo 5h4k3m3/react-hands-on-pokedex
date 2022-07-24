1b4d811ecb25fa8f6308d2e6eedb198cd35581b9
SalmonFillet <sy4.l3ucas@gmail.com>
2022-07-24 13:32:17 +0900

    ✨ impl loading

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import { getAllPokemon } from "./utils/pokemon";
 import "./App.css";
 
 export const App = () => {
   const initialURL = "https://pokeapi.co/api/v2/pokemon";
-
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
     const fetchPokemonData = async () => {
       //fetch all poke data
       const res = await getAllPokemon(initialURL);
       console.log(res);
+      setLoading(false);
     };
     fetchPokemonData();
   }, []);
+
+  return (
+    <div className="app">
+      {loading ? <h1>Loading...</h1> : <h1>Get PokeData</h1>}
+    </div>
+  );
 };
