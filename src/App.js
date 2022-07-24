ff9fea11cf8d08cdd80abe37ebe66d03d6822321
SalmonFillet <sy4.l3ucas@gmail.com>
2022-07-24 13:42:00 +0900

    ✨ fetch each poke data

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
 import { useState, useEffect } from "react";
-import { getAllPokemon } from "./utils/pokemon";
+import { getAllPokemon, getPokemon } from "./utils/pokemon";
 import "./App.css";
 
 export const App = () => {
   const initialURL = "https://pokeapi.co/api/v2/pokemon";
   const [loading, setLoading] = useState(true);
+  const [pokemonData, setPokemonData] = useState([]);
+
   useEffect(() => {
     const fetchPokemonData = async () => {
       //fetch all poke data
       const res = await getAllPokemon(initialURL);
-      console.log(res);
+      //fetch each poke data
+      loadPokemon(res.results);
+
       setLoading(false);
     };
     fetchPokemonData();
   }, []);
+
+  const loadPokemon = async (data) => {
+    const _pokemonData = await Promise.all(
+      data.map((pokemon) => {
+        const pokemonRecord = getPokemon(pokemon.url);
+        return pokemonRecord;
+      })
+    );
+    setPokemonData(_pokemonData);
+  };
+
+  console.log(pokemonData);
 
   return (
     <div className="app">
